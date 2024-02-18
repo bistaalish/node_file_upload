d99739daae8f2b4069d85625a991a50a60dfece0
Alish Bista <bistace321@gmail.com>
2024-02-18 19:10:13 +0545

    Feat: Initated demo page

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 //  Import required Modules
-const express = require("express")
+const express = require("express");
+const multer = require("multer");
+const path = require('path');
+
+// Create Express app
+const app = express();
+const port = process.env.PORT || 3000; // Use PORT from .env file or default to 3000
+
+app.get("/",(req,res)=>{
+    res.send("Welcome dude");
+})
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
